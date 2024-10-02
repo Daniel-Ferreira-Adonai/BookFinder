d7f44a15fbe65cfa2d51269550d98d212588b6cf
daniel felix <Daniel-Ferreira-Adonai>
2024-10-02 10:14:25 -0300

    Project Spine

--- app/src/main/java/com/example/BookFinder/MainActivity.kt
+++ app/src/main/java/com/example/BookFinder/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.BookFinder
 
 import android.annotation.SuppressLint
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,8 +22,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
@@ -28,11 +35,15 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.PeopleAlt
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -44,8 +55,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -96,6 +110,7 @@ private fun app() {
         navItem.community
 
         )
+    
     var selectedItem by remember {
         mutableStateOf(navItems.first())
     }
@@ -202,9 +217,20 @@ private fun app() {
         }
     }
 }
-
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun cardMeme(@DrawableRes drawable: Int){ Image(
+    painter = painterResource(drawable),
+    contentDescription = null,
+    contentScale = ContentScale.Crop,
+    modifier = Modifier
+        .size(88.dp)
+        .clip(CircleShape)
+)
+}
+
+// do not include this
+@Composable
+fun MainScreen(modifier: Modifier = Modifier,) {
     Box(modifier.fillMaxSize()) {
         Text(
             "Main Screen",
@@ -213,6 +239,8 @@ fun MainScreen(modifier: Modifier = Modifier) {
         )
     }
 }
+
+
 @Composable
 fun MapScreen(modifier: Modifier = Modifier) {
     Box(modifier.fillMaxSize()) {
@@ -243,6 +271,29 @@ fun CommunityScreen(modifier: Modifier = Modifier) {
         )
     }
 }
+
+// this is for test
+@Composable
+
+fun cardWithName(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { /* TODO */ }
+            ) {
+                Text("Show more")
+            }
+        }
+    }
+}
+// this is for test
 
 @Preview
 @Composable
